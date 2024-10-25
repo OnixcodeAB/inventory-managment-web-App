d23b45cc8010814b4165c93aefb91a3174ef73da
onixcodeAB <onixcode.AB@gmail.com>
2024-10-25 12:51:23 -0400

    feat: Update auth services to use async/await for database operations

--- server/src/api/auth/auth.services.js
+++ server/src/api/auth/auth.services.js
@@ -2,9 +2,13 @@ import { db } from "../../utils/db.js";
 import { hashToken } from "../../utils/hashToken.js";
 
 // used when we create a refresh token
-export function addRefreshTokenToWhitelist({ jti, refreshToken, userId }) {
+export async function addRefreshTokenToWhitelist({
+  jti,
+  refreshToken,
+  userId,
+}) {
   try {
-    return db.refreshToken.create({
+    return await db.refreshToken.create({
       data: {
         id: jti,
         hashedToken: hashToken(refreshToken),
@@ -17,27 +21,39 @@ export function addRefreshTokenToWhitelist({ jti, refreshToken, userId }) {
 }
 
 // used to check if the token sent by the client is in the database
-export function findRefreshTokenById(id) {
-  return db.refreshToken.findUnique({ where: { id } });
+export async function findRefreshTokenById(id) {
+  try {
+    return await db.refreshToken.findUnique({ where: { id } });
+  } catch (error) {
+    return error;
+  }
 }
 
 // soft delete token after usage.
-export function deleteRefreshToke(id) {
-  return db.refreshToken.update({
-    where: { id },
-    data: {
-      revoked: true,
-    },
-  });
+export async function deleteRefreshToke(id) {
+  try {
+    return await db.refreshToken.update({
+      where: { id },
+      data: {
+        revoked: true,
+      },
+    });
+  } catch (error) {
+    return error;
+  }
 }
 
-export function revokeTokens(userId) {
-  return db.refreshToken.updateMany({
-    where: {
-      userId,
-    },
-    data: {
-      revoked: true,
-    },
-  });
+export async function revokeTokens(userId) {
+  try {
+    return await db.refreshToken.updateMany({
+      where: {
+        userId,
+      },
+      data: {
+        revoked: true,
+      },
+    });
+  } catch (error) {
+    return error;
+  }
 }
