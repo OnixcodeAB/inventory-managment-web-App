744ac6277bc5a6d09784b691499c5a8dc142a6f1
onixcodeAB <onixcode.AB@gmail.com>
2025-01-03 11:23:44 -0400

    fix: Handle missing JWT keys by returning error instead of throwing

--- server/src/utils/jwt.js
+++ server/src/utils/jwt.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 // Usually  keep the token between 5 minutes - 15 minutes
 export function generateAccessToken(user) {
   if (!process.env.JWT_ACCESS_SECRET) {
-    throw new Error("Missing JWT Access key ");
+    return { error: "Missing JWT Access key " };
   }
   return jwt.sign({ userId: user.id }, process.env.JWT_ACCESS_SECRET, {
     expiresIn: "1h",
@@ -12,7 +12,7 @@ export function generateAccessToken(user) {
 
 export function generateRefreshToken(user, jti) {
   if (!process.env.JWT_REFRESH_SECRET) {
-    throw new Error("Missing JWT Refresh key ");
+    return { error: "Missing JWT Refresh key " };
   }
   return jwt.sign(
     {
@@ -29,7 +29,7 @@ export function generateRefreshToken(user, jti) {
 export function generateTokens(user, jti) {
   const accessToken = generateAccessToken(user);
   const refreshToken = generateRefreshToken(user, jti);
-  console.log({ accessToken, refreshToken });
+  //console.log({ accessToken, refreshToken });
   return {
     accessToken,
     refreshToken,
